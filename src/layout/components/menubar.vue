38f4f8d02ddd2b9978c07ed191c28edb10ae6133
nero.wang <bakujin@qq.com>
2021-08-11 19:18:26 +0800

    update 修改左侧菜单为有赞双栏样式

--- src/layout/components/menubar.vue
+++ src/layout/components/menubar.vue
@@ -1,77 +1,146 @@
 <template>
-    <el-scrollbar wrap-class='scrollbar-wrapper'>
-        <el-menu
-            :default-active='activeMenu'
-            :collapse='menubar.status === 1 || menubar.status === 3'
-            :class='{ 
-                "el-menu-vertical-demo": true,
-                "w-64": menubar.status === 0 || menubar.status === 2, 
-                "w-0": menubar.status === 3, 
-                "w-16": menubar.status === 1, 
-            }'
-            :collapse-transition='false'
-            :unique-opened='true'
-            @select='onOpenChange'
-        >
-            <menubar-item v-for='v in filterMenubarData' :key='v.path' :index='v.path' :menu-list='v' />
-        </el-menu>
+  <div class="flex">
+    <el-menu
+      active-text-color="#ffd04b"
+      background-color="#202c30"
+      :class="{
+        'ele-override': true,
+        'el-menu-vertical-demo': true,
+        'w-20': menubar.status === 0 || menubar.status === 2,
+        'w-0': menubar.status === 3,
+        'w-0': menubar.status === 1,
+      }"
+      :default-active="rootActiveMenu"
+      text-color="#fff"
+      @select="(idx) => (selectroot = idx)"
+    >
+      <menubar-item v-for="v in rootMenus" :key="v.path" :index="v.path" :menu-list="v" />
+    </el-menu>
+    <el-scrollbar class="whitelize" wrap-class="scrollbar-wrapper">
+      <el-menu
+        :class="{
+          'el-menu-vertical-demo': true,
+          'w-44': menubar.status === 0 || menubar.status === 2,
+          'w-0': menubar.status === 3,
+          'w-16': menubar.status === 1,
+        }"
+        :collapse="menubar.status === 1 || menubar.status === 3"
+        :collapse-transition="false"
+        :default-active="activeMenu"
+        :default-openeds="allOpenedIndex"
+        @select="onOpenChange"
+      >
+        <menubar-item v-for="v in rightMenus" :key="v.path" :index="v.path" :menu-list="v" />
+      </el-menu>
     </el-scrollbar>
+  </div>
 </template>
 
-<script lang='ts'>
-import { defineComponent, computed } from 'vue'
+<script lang="ts">
+import { defineComponent, computed, ref } from 'vue'
 import MenubarItem from '/@/layout/components/menubarItem.vue'
 import { useRoute, useRouter } from 'vue-router'
 import { IMenubarList } from '/@/type/store/layout'
 import { useLayoutStore } from '/@/store/modules/layout'
 
 // 过滤隐藏的菜单，并提取单条的子菜单
-const filterMenubar = (menuList:IMenubarList[]) => {
-    const f = (menuList:IMenubarList[]) => {
-        let arr:IMenubarList[] = []
-        menuList.filter(v => !v.meta.hidden).forEach(v => {
-            let child = v.children && v.children.filter(v => !v.meta.hidden)
-            let currentItem = v
-            if(!v.meta.alwaysShow && child && child.length === 1) {
-                [currentItem] = child
-            }
-            arr.push(currentItem)
-            if(currentItem.children && currentItem.children.length > 0) {
-                arr[arr.length - 1].children = f(currentItem.children)
-            }
-        })
-        return arr
-    }
-    return f(menuList)
+const filterMenubar = (menuList: IMenubarList[]) => {
+  const f = (menuList: IMenubarList[]) => {
+    let arr: IMenubarList[] = []
+    menuList
+      .filter((v) => !v.meta.hidden)
+      .forEach((v) => {
+        let child = v.children && v.children.filter((v) => !v.meta.hidden)
+        let currentItem = v
+        if (!v.meta.alwaysShow && child && child.length === 1) {
+          [currentItem] = child
+        }
+        arr.push(currentItem)
+        if (currentItem.children && currentItem.children.length > 0) {
+          arr[arr.length - 1].children = f(currentItem.children)
+        }
+      })
+    return arr
+  }
+  return f(menuList)
 }
 
-export default defineComponent ({
-    name: 'LayoutMenubar',
-    components: {
-        MenubarItem
-    },
-    setup() {
-        const route = useRoute()
-        const router = useRouter()
-        const { getMenubar, setRoutes, changeCollapsed } = useLayoutStore()
+export default defineComponent({
+  name: 'LayoutMenubar',
+  components: {
+    MenubarItem,
+  },
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const { getMenubar, setRoutes, changeCollapsed } = useLayoutStore()
 
-        const filterMenubarData = filterMenubar(getMenubar.menuList)
-        setRoutes(filterMenubarData)
+    const filterMenubarData = filterMenubar(getMenubar.menuList)
+    setRoutes(filterMenubarData)
 
-        const activeMenu = computed(() => {
-            if(route.meta.activeMenu) return route.meta.activeMenu
-            return route.path
-        })
-        const onOpenChange = (d: any) => {
-            router.push({ path: d })
-            getMenubar.status === 2 && changeCollapsed()
-        }
-        return {
-            menubar: getMenubar,
-            filterMenubarData,
-            activeMenu,
-            onOpenChange
-        }
+    const activeMenu = computed(() => {
+      if (route.meta.activeMenu) return route.meta.activeMenu
+      return route.path
+    })
+    let selectroot: any = ref(null)
+    const rootActiveMenu = computed(() => {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      selectroot.value = null
+      return activeMenu.value && `/${(activeMenu.value as any as string).split('/')[1]}`
+    })
+    const onOpenChange = (d: any) => {
+      router.push({ path: d })
+      getMenubar.status === 2 && changeCollapsed()
     }
+    // 左侧一级菜单的数据
+    let rootMenus = computed(() =>
+      filterMenubarData.map((c) => {
+        let d = { ...c }
+        d.children = undefined
+        d.meta.title = c.meta.title.substr(0, 3)
+        return d
+      })
+    )
+    // 右侧二级菜单的数据
+    let rightMenus = computed(() => {
+      if (getMenubar.status == 0 || getMenubar.status == 2) {
+        let selected = filterMenubarData.filter((c) => c.path == (selectroot.value || rootActiveMenu.value))
+        return (selected[0] && selected[0].children) || filterMenubarData[0].children || []
+      } else {
+        return filterMenubarData
+      }
+    })
+    let allOpenedIndex: any = []
+    filterMenubarData.forEach((c) => {
+      if (c.children) allOpenedIndex.push(...c.children?.map((c) => c.path))
+    })
+
+    return {
+      selectroot,
+      menubar: getMenubar,
+      filterMenubarData,
+      rootMenus,
+      rightMenus,
+      allOpenedIndex,
+      rootActiveMenu,
+      activeMenu,
+      onOpenChange,
+    }
+  },
+  mounted() {
+    this.selectroot = this.rootActiveMenu
+  },
 })
 </script>
+<style lang="postcss">
+.ele-override {
+  .el-menu-item {
+    padding: 0 8px !important;
+    font-size: 0.7rem;
+    i {
+      width: 0.7rem;
+      font-size: 0.7rem;
+    }
+  }
+}
+</style>
