ff471d1820611b4c6aa8dbb905538e9acf0342e1
nero.wang <bakujin@qq.com>
2021-08-12 17:35:03 +0800

    update 修改认证方式为jwt标准认证的header

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,52 +3,52 @@ import axios from 'axios'
 import { AxiosResponse } from 'axios'
 import { ElLoading, ElNotification } from 'element-plus'
 
-let loading:{close():void}
+let loading: { close(): void }
 // 创建 axios 实例
 const request = axios.create({
   // API 请求的默认前缀
   baseURL: import.meta.env.VUE_APP_API_BASE_URL as string | undefined,
-  timeout: 60000 // 请求超时时间
+  timeout: 60000, // 请求超时时间
 })
 
 // 异常拦截处理器
-const errorHandler = (error:{message:string}) => {
+const errorHandler = (error: { message: string }) => {
   loading.close()
   console.log(`err${error}`)
   ElNotification({
     title: '请求失败',
     message: error.message,
-    type: 'error'
+    type: 'error',
   })
   return Promise.reject(error)
 }
 
 // request interceptor
-request.interceptors.request.use(config => {
+request.interceptors.request.use((config) => {
   const { getStatus } = useLayoutStore()
   loading = ElLoading.service({
     lock: true,
     text: 'Loading',
     spinner: 'el-icon-loading',
-    background: 'rgba(0, 0, 0, 0.4)'
+    background: 'rgba(0, 0, 0, 0.4)',
   })
   const token = getStatus.ACCESS_TOKEN
   // 如果 token 存在
   // 让每个请求携带自定义 token 请根据实际情况自行修改
   if (token) {
-    config.headers['Access-Token'] = token
+    config.headers['Authorization'] = token
   }
   return config
 }, errorHandler)
 
 // response interceptor
-request.interceptors.response.use((response:AxiosResponse<IResponse>) => {
+request.interceptors.response.use((response: AxiosResponse<IResponse>) => {
   const { data } = response
   const { getStatus, logout } = useLayoutStore()
   loading.close()
-  if(data.Code !== 200) {
+  if (data.Code !== 200) {
     let title = '请求失败'
-    if(data.Code === 401) {
+    if (data.Code === 401) {
       if (getStatus.ACCESS_TOKEN) {
         logout()
       }
@@ -57,7 +57,7 @@ request.interceptors.response.use((response:AxiosResponse<IResponse>) => {
     ElNotification({
       title,
       message: data.Msg,
-      type: 'error'
+      type: 'error',
     })
     return Promise.reject(new Error(data.Msg || 'Error'))
   }
